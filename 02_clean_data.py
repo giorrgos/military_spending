bdfb05db315a4e7c82c29662996e61f6e6c07e63
giorrgos <38963601+giorrgos@users.noreply.github.com>
2023-09-22 18:35:42 +0100

    renaming files with new convention

--- 02_clean_data.py
+++ 02_clean_data.py
@@ -19,15 +19,20 @@ clean_military_data = get_wb_military.rename(
         'economy':'country_iso3', 
         'time':'year',
         'MS.MIL.XPND.CD':'milex_usd',
+        'MS.MIL.XPND.GD.ZS':'milex_pct',
+        'MS.MIL.XPND.ZS':'milex_gov_exp_pct',
         'SP.POP.TOTL':'population'
-        })
-
+        }
+    )
 
 # Join region
 clean_military_data = clean_military_data.merge(
     map_country_region[['country_iso3', 'country_name', 'region_name_wb']], 
     how='left', left_on='country_iso3', right_on='country_iso3'
     )
+
+clean_military_data.info()
+df = clean_military_data
 
 # Write to csv
 clean_military_data.to_csv('wb_data/clean_military_data.csv')
---
+++ app.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+
+# https://blog.streamlit.io/how-to-build-a-real-time-live-dashboard-with-streamlit/
+
+# Load data
+df = pd.read_csv('wb_data/clean_military_data.csv')
+
+max_year = df['year'].max()
+
+# Dashboard config
+st.set_page_config(
+    page_title="Military Expenditure",
+    page_icon="✅",
+    layout="wide",
+)
+
+
+# Use a get_data() function so it is properly cached
+@st.cache_data
+def get_data() -> pd.DataFrame:
+    return pd.read_csv(df)
+
+
+# Create the UI
+
+# Page title
+st.title("Military Expenditure Analysis")
+
+st.sidebar.multiselect("Select Year:", pd.unique(df["year"]))
+st.sidebar.multiselect("Select Country:", pd.unique(df["country_name"]))
+st.sidebar.multiselect("Select Region:", pd.unique(df["region_name_wb"]))
+
+kpi_total_milex_max_year, kpi_avg_milex_gdp_pct_max_year, kpi_avg_milex_gov_exp_pct_max_year = st.columns(3)
+
+# KPI 1
+kpi_total_milex_max_year.metric( 
+    label=f"Total Military Expenditure for {max_year} in ($b)",
+    value=round(df[df['year'] == max_year]['milex_usd'].sum()/1000000000, 1)
+) 
+
+
+# KPI 2
+kpi_avg_milex_gdp_pct_max_year.metric(
+      label=f"Average Military Expenditure as % of GDP for {max_year} in ($b)",
+      value=df[df['year'] == max_year]['milex_pct'].mean()
+)
+
+# KPI 3
+kpi_avg_milex_gov_exp_pct_max_year.metric(
+    label=f"Average Military Expenditure as % of Gov Exp for {max_year} in ($b)",
+    value=df[df['year'] == max_year]['milex_gov_exp_pct'].mean(),
+)
